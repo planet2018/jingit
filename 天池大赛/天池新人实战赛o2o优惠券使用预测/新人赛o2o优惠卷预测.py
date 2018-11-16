c8fc9ea1e8b1553e73d543123598f523bd326808
planet2018 <385120573@qq.com>
2018-11-16 23:12:53 +0800

    test

---
+++ 天池大赛/天池新人实战赛o2o优惠券使用预测/新人赛o2o优惠卷预测.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 16 22:57:01 2018
+
+@author: jinyong
+"""
+import numpy as np
+import pandas as pd
+
+offline_train = pd.read_csv("data/ccf_offline_stage1_train.csv")
+online_train = pd.read_csv("data/ccf_online_stage1_train.csv")
+offline_test = pd.read_csv("data/ccf_offline_stage1_test_revised.csv")
